1e9742d48c76c716c4f32099e69cf413191e60d4
Stefano <stefano_canossini@hotmail.com>
2020-07-22 19:24:26 -0300

    react

---
+++ resources/views/spa.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>React</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="example">
+    </div>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    
+    
+</body>
+</html>
