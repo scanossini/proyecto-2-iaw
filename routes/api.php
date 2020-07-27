0038a0b5e7a5d3f56c0461c5855de0920dc8bda8
Stefano <stefano_canossini@hotmail.com>
2020-07-27 01:15:48 -0300

    middleware

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('donante', 'DonantesController@list');
+Route::middleware('auth:api')->get('donante', 'DonantesController@list');
 
-Route::get('donante/{id}', 'DonantesController@get');
+Route::middleware('auth:api')->get('donante/{id}', 'DonantesController@get');
 
